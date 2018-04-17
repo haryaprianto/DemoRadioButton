2eeb970b05de63685d1980eb3e03812860b6833a
haryaprianto <16022596@myrp.edu.sg>
2018-04-17 14:36:33 +0800

    Update on activity_main and MainActivity

--- app/src/main/java/com/example/a16022596/demoradiobutton/MainActivity.java
+++ app/src/main/java/com/example/a16022596/demoradiobutton/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.a16022596.demoradiobutton;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnOk;
+    RadioGroup rg;
+    RadioButton rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnOk = (Button)findViewById(R.id.button1);
+        rg = (RadioGroup) findViewById(R.id.radioGroup1);
+
+
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedButtonId = rg.getCheckedRadioButtonId();
+                rb = (RadioButton)findViewById(selectedButtonId);
+                Toast.makeText(getBaseContext(),rb.getText(),Toast.LENGTH_LONG).show();
+
+
+
+            }
+        });
     }
 }
